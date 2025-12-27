4c03402f34aa7e0c2d7c34db4d06fd0260171853
Aayush <aayushyt980@gmail.com>
2025-12-27 22:21:36 +0530

    Optimize twoSum function using HashMap
    Refactor twoSum function to use a HashMap for optimal solution.

--- Two Sum.cpp
+++ Two Sum.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+       /* //Non Optimal solution
         vector<int>result;
         for(auto it1 = nums.begin(); it1 != nums.end(); ++it1){
             auto it2 = find(it1+1, nums.end(), target - *it1);
@@ -9,7 +10,18 @@ public:
                 result.push_back(it2 - nums.begin());
                 break;
             }
+        }*/
+        unordered_map<int, int> _map;  //Optimal Aolution using HashMap
+        for(int i = 0; i< nums.size(); ++i){
+            int num = nums[i];
+            int comp = target - num ;
+            auto it = _map.find(comp);
+            if (it != _map.end()){
+                return {it ->second, i};
+            }
+            _map[num] = i;
         }
-        return result;
+        return {};
     }
+
 };
